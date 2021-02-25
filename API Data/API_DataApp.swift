d6d74659127aab770281214d38b9f0f8d5643911
Darren Darlington <darrend42@outlook.com>
2021-02-25 10:18:27 -0600

    Initial Commit

---
+++ API Data/API_DataApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_DataApp.swift
+//  API Data
+//
+//  Created by Darren Darlington on 2/25/21.
+//
+
+import SwiftUI
+
+@main
+struct API_DataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
